e4005e39ac31dd32b808cf62412006ea90c814ea
snippywippy <austin.l.hill@gmail.com>
2026-09-19 17:09:11 -0500

    Added favicons

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 // Cache the shell so the app opens instantly; never cache API results.
-const CACHE = 'gff-v1';
-const SHELL = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE = 'gff-v2';
+const SHELL = ['/', '/index.html', '/manifest.json', '/favicon.ico',
+  '/icons/icon-32.png', '/icons/icon-48.png', '/icons/icon-180.png',
+  '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
